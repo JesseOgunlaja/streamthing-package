a26d2d7b5661e5a35b631f9a2a416f831c87eadb
JesseOgunlaja <jesseogunlaja@gmail.com>
2025-02-14 22:54:16 +0000

    feat: add proper types in the decleration file

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,35 @@
-declare module "streamthing";
+type ServerStreamConfig = {
+  id: string;
+  region: string;
+  channel: string;
+  password: string;
+  encryptionKey: string;
+  receiving?: boolean;
+};
+
+type ServerStreamReturn = {
+  send: (event: string, msg: string) => Promise<void>;
+  receive?: (event: string, callback: (data: unknown) => unknown) => void;
+  disconnect?: () => void;
+};
+
+export function createServerStream(
+  config: ServerStreamConfig
+): ServerStreamReturn {}
+
+type ClientStreamConfig = {
+  id: string;
+  region: string;
+  channel: string;
+  password: string;
+  encryptionKey: string;
+};
+
+type ClientStreamReturn = {
+  receive: (event: string, callback: (data: unknown) => unknown) => void;
+  disconnect: () => void;
+};
+
+export function createClientStream(
+  config: ClientStreamConfig
+): ClientStreamReturn {}
